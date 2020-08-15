f8ee6d258a1f3ab8f37e6431141a61dfea5e5633
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-08-15 11:24:53 +0900

    fadeIn,hover

--- app/assets/javascripts/text.js
+++ app/assets/javascripts/text.js
@@ -1,3 +1,9 @@
-$(function(){
-  $(".top-text")
+$(function() {
+  $('.top-text').hide().fadeIn(8000);
+  $('.top-text-2').hide().fadeIn(10000);
+  $('.post-text,.icon-text-2').hover(function() {
+    $(this).css('color', '#c00');
+  }, function() {
+    $(this).css('color', '');
+  });
 });
