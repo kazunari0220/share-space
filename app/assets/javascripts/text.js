a20ee2f72a62a7b42afa2f19a2ec43013402c9c0
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-08-16 21:35:08 +0900

    js修正

--- app/assets/javascripts/text.js
+++ app/assets/javascripts/text.js
@@ -1,7 +1,7 @@
 $(function() {
-  $('.top-image').hide().fadeIn(5000);
-  $('.top-text').hide().fadeIn(8000);
-  $('.top-text-2').hide().fadeIn(10000);
+  $('.top-image').hide().fadeIn(3000);
+  $('.top-text').hide().fadeIn(5000);
+  $('.top-text-2').hide().fadeIn(8000);
   $('.post-text,.icon-text-2,.icon-text-2-2').hover(function() {
     $(this).css('color', '#c00');
   }, function() {
