4335ed203cf348322eaf6cdd8c9a02aa0f21c1ac
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-08-18 16:22:22 +0900

    mysql

--- db/migrate/20200710175723_create_posts.rb
+++ db/migrate/20200710175723_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
       t.string :name
       t.string :title
       t.text :text
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
